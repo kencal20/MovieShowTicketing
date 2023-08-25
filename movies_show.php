7c575b0527209116e99663a0d53c05050b352c14
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-08-25 19:58:59 +0000

    movie_show  still on going

--- movies_show.php
+++ movies_show.php
@@ -1,18 +1,34 @@
 <?php
-include 'inc/header.php';
+    include 'inc/header.php';
 
 ?>
 
 <main>
   <div class="container d-flex flex-column align-items-center">
-
+   
     <h2>Movie Shows</h2>
-    <div class="img-thumbnail">
-      <img height="300px" width="300px" src="./uploads/Landscape-Color.jpg" alt="...">
-      <hr>
-      <p>Lorem ipsum dolor sit amet <br/> consectetur adipisicing elit. </p>
-    </div>
-    <?php
+
+    <div class="card my-3">
+     <div class="card-body">
+       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+     </div>
+   </div>
+
+   <div class="card my-3">
+     <div class="card-body">
+       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+     </div>
+   </div>
+
+   <div class="card my-3">
+     <div class="card-body">
+       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+     </div>
+   </div>
+  </div>
+</main>
+
+<?php
     include 'inc/footer.php';
 
-    ?>
+?>
